2d92d5d24f2136422551755a42a0dd979d7edce8
deivid145 <136328840+deivid145@users.noreply.github.com>
2023-11-27 08:56:34 -0300

    Add files via upload

---
+++ aluramidi-curso-arquivos-iniciais/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,8 @@
+//pom-----------------------------------------------------
+function tocasompom () {
+    document.querySelector('#som_tecla_pom').play()
+}
+
+const listaDeTeclas = document.querySelectorAll('tecla')
+
+
